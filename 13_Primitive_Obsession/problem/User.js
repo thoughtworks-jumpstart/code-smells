164b3e05f86e13f9bda7120dc9ea45c44e1851fb
Angeline <atan@thoughtworks.com>
2019-04-22 14:15:02 +0800

    Modify Example 13 to use Address object

--- 13_Primitive_Obsession/problem/User.js
+++ 13_Primitive_Obsession/problem/User.js
@@ -1,5 +1,12 @@
 class User {
-  constructor(name){
-    this.name = name
+  constructor(firstName, lastName, age, gender, block, streetName, unitNo, postalCode){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.gender = gender
+    this.block = block
+    this.streetName = streetName
+    this.unitNo = unitNo
+    this.postalCode = postalCode
   }
 }
--- 13_Primitive_Obsession/solution/User.js
+++ 13_Primitive_Obsession/solution/User.js
@@ -1,6 +1,9 @@
 class User {
-  constructor(firstName, lastName) {
-    this.name = new Name(firstName, lastName);
+  constructor(name, age, gender, address) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.address = address;
   }
 }
 
@@ -13,3 +16,19 @@ class Name {
     return `${this.firstName} ${this.lastName}`;
   }
 }
+
+class Address {
+  constructor(block, streetName, unitNo, postalCode) {
+    this.block = block;
+    this.streetName = streetName;
+    this.unitNo = unitNo;
+    this.postalCode = postalCode;
+  }
+}
+
+const user1 = new User(
+  new Name(first, last),
+  age,
+  gender,
+  new Address("122", "Costa East Rd", "#19-02", "345643")
+);
